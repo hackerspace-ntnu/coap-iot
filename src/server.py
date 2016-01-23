0879b6eef410d60c05e957116473332cb5d4f1e3
Andreas Friestad <lemonous@dhcp-072241.wlan.ntnu.no>
2016-01-23 17:11:13 +0100

    Add Flask server in a new tread, move web files to follow Flask convention (more or less)

--- src/server.py
+++ src/server.py
@@ -1,8 +1,15 @@
 import logging
 import asyncio
+import flask
+import threading
 
 import aiocoap.resource as resource
 import aiocoap
+
+app = flask.Flask(__name__,static_folder="../static",static_url_path="/static",template_folder="../templates")
+@app.route("/")
+def hello():
+    return flask.render_template("index.html", name="index")
 
 class BlockResource(resource.Resource):
     """
@@ -32,10 +39,14 @@ def main():
     root = resource.Site()
     root.add_resource(('block',),BlockResource())
     
+    websrv = threading.Thread(target=(lambda: app.run(debug=True, port=25565, use_reloader=False)), name="Flask-server")
+    websrv.start()
+    
     asyncio.async(aiocoap.Context.create_server_context(root))
     asyncio.get_event_loop().run_forever()
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
-main()
+if __name__ == "__main__":
+    main()
